960974fbfde1eddc2f57c386196c0932dd26bb43
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-06 22:53:57 +0530

    add axios package

--- fronend/src/App.jsx
+++ fronend/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [books, setbooks] = useState([]);
 
   return (
     <>
-   <h1>UI is Running...</h1>
+      <h1>UI is Running...</h1>
+      <p>BOOKS: {books.length}</p>
+
+      {books.map((book, index) => {
+        <div key={book.id}>
+          <h3>{book.name}</h3>
+          <h3>{book.author}</h3>
+        </div>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
