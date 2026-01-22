8d71a2f64954970d25b21c485966274ebfefc7df
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-22 17:58:23 +0530

    add request and response to httpServer code

--- core_node.js/http_server/index.js
+++ core_node.js/http_server/index.js
@@ -1,6 +1,12 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req,res)=>{
+    // res.send('I got a incoming request')
+    console.log("I got a incoming request")
+    res.writeHead(200);
+    res.end("Thanks for visiting my server")
+
+});
 
 server.listen(8000,()=>{
     console.log(`server is listening on port 8000`);
