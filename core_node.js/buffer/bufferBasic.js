a37dd6d5d6bf7edf43c0a8b4fc48ec3c5005173f
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-22 16:53:16 +0530

    practice some more methods in buffer

--- core_node.js/buffer/bufferBasic.js
+++ core_node.js/buffer/bufferBasic.js
@@ -1,4 +1,17 @@
 const {Buffer} = require("buffer");
+const { log } = require("console");
 
-const buf = Buffer.alloc(4);
-console.log(buf);
+// const buf = Buffer.alloc(4);
+// console.log(buf);
+
+// const buf = Buffer.from('Hello bappaditya');
+// console.log(buf)
+// console.log(buf.toString());
+
+
+// const buf2 = Buffer.allocUnsafe(10);
+// console.log(buf2);
+
+const buf = Buffer.alloc(10);
+buf.write('Hello');
+console.log(buf.toString());
