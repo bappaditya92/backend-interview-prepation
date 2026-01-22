403775d047d7f5e15bc5fcb26881af415a831cf9
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-22 17:03:09 +0530

    practice some buffer code

--- core_node.js/buffer/bufferBasic.js
+++ core_node.js/buffer/bufferBasic.js
@@ -16,7 +16,16 @@ const { log } = require("console");
 // buf.write('Hello');
 // console.log(buf.toString());
 
-const buf = Buffer.from("Hello from Bappaditya");
-console.log(buf.toString());
-console.log(buf.toString("utf-8", 0,10));
+const buf = Buffer.from("Hello");
+// console.log(buf.toString());
+// console.log(buf.toString("utf-8", 0,10));
+// buf[0] = 0x4A;
+// console.log(buf);
+// console.log(buf.toString())
 
+const buf1 = Buffer.from(" world");
+
+const merged = Buffer.concat([buf,  buf1])
+console.log(merged);
+console.log(merged.toString());
+console.log(merged.length);
