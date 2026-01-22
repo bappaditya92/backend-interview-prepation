b0371390a996a444525583d4fd655e4551c528d6
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-22 16:56:01 +0530

    add some code

--- core_node.js/buffer/bufferBasic.js
+++ core_node.js/buffer/bufferBasic.js
@@ -12,6 +12,11 @@ const { log } = require("console");
 // const buf2 = Buffer.allocUnsafe(10);
 // console.log(buf2);
 
-const buf = Buffer.alloc(10);
-buf.write('Hello');
+// const buf = Buffer.alloc(10);
+// buf.write('Hello');
+// console.log(buf.toString());
+
+const buf = Buffer.from("Hello from Bappaditya");
 console.log(buf.toString());
+console.log(buf.toString("utf-8", 0,10));
+
