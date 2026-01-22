47921c6ba92489c7d325fbcc323db188cc87c2be
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-22 21:42:02 +0530

    create a advance server

--- core_node.js/HTTP_02/index.js
+++ core_node.js/HTTP_02/index.js
@@ -4,6 +4,25 @@ const server = http.createServer((req,res)=>{
     console.log(`new request comming at [${Date.now().toString()}]`)
     console.log(req.headers);
     console.log(req.method)
+    console.log(req.url)
+    switch(req.url){
+        case '/':
+            res.writeHead(200);
+            return res.end('Homepage')
+            break
+        case '/contact-us':
+            res.writeHead(200)
+            return res.end('contact me at bappadityasinha.92@gmail.com')
+            break
+        case '/about':
+            res.writeHead(200)
+            return res.end('I am software engineer')
+            break
+        default:
+            res.writeHead(404)
+            return res.end('You are lost');
+
+    }
 
     res.writeHead(201);
     res.end(`Hey, You can accept ${req.headers['accept-language']}`)
