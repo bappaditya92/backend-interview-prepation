ec8013c864eb2915fb9ee5ffb2ebd6f30b517379
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-04 12:17:01 +0530

    add post api

--- express/index.js
+++ express/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.get('/', (req,res)=>{
     res.end('app is running')
+});
+
+app.get('/contact-us', (req,res)=>{
+    res.end('you can contact me with my email address')
+})
+
+app.post('/tweets', (req,res)=>{
+    res.status(201).end('Tweet created successfully')
 })
 
 app.listen(3000, ()=>{
