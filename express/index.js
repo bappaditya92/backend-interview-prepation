aa816f05a5f6869597b9525093070ee7c7a1324f
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-04 13:44:37 +0530

    formatting code

--- express/index.js
+++ express/index.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.end('app is running');
+app.get("/", (req, res) => {
+  res.end("app is running");
 });
 
-app.get('/contact-us', (req,res)=>{
-    res.end('you can contact me with my email address');
+app.get("/contact-us", (req, res) => {
+  res.end("you can contact me with my email address");
 });
 
-app.post('/tweets', (req,res)=>{
-    res.status(201).end('Tweet created successfully');
+app.post("/tweets", (req, res) => {
+  res.status(201).end("Tweet created successfully");
+  
 });
 
-app.get('/tweets',(req,res)=>{
-    res.end('your tweet is here');
+app.get("/tweets", (req, res) => {
+  res.end("your tweet is here");
 });
 
-app.listen(8000, ()=>{
-    console.log(`app is running on 3000`);
+app.listen(8000, () => {
+  console.log(`app is running on 3000`);
 });
