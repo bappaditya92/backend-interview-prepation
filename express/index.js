2f788af025481d1fdf51f945c23bd5624341a212
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-04 12:20:50 +0530

    added GET API and implemented best practices

--- express/index.js
+++ express/index.js
@@ -3,17 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.end('app is running')
+    res.end('app is running');
 });
 
 app.get('/contact-us', (req,res)=>{
-    res.end('you can contact me with my email address')
-})
+    res.end('you can contact me with my email address');
+});
 
 app.post('/tweets', (req,res)=>{
-    res.status(201).end('Tweet created successfully')
-})
+    res.status(201).end('Tweet created successfully');
+});
 
-app.listen(3000, ()=>{
-    console.log(`app is running on 3000`)
-})
+app.get('/tweets',(req,res)=>{
+    res.end('your tweet is here');
+});
+app.listen(8000, ()=>{
+    console.log(`app is running on 3000`);
+});
