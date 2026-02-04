8a762a957a8e3588a9f5c7234755261279cd223b
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-04 13:57:02 +0530

    add books get api

--- express_bookApi/index.js
+++ express_bookApi/index.js
@@ -1,14 +1,38 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
-
 const PORT = 8000;
 
+//In memory database
 
-app.get('/', (req, res)=>{
-    res.end('server is running...');
+const books = [
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    price: 450,
+    available: true,
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: 350,
+    available: true,
+  },
+  {
+    id: 3,
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
+    price: 550,
+    available: false,
+  },
+];
+
+//Route
+app.get("/books", (req, res) => {
+  res.json(books)
 });
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
