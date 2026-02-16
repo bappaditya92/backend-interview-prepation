3806831a010618e6e5ff12c3a27cb6cf9d5f9b18
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-16 23:53:59 +0530

    fix a bug in routes file

--- express_bookApi/routes/route.book.js
+++ express_bookApi/routes/route.book.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controllers/book.controller')
 
-router.get(controller.getAllBooks);
+router.get('/', controller.getAllBooks);
 router.get('/:id', controller.getAllBooksById)
 router.post("/", controller.createBook)
 router.delete("/:id", controller.deleteBookById)
