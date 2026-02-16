9037cb07d1a6683b02c5fc797f40f447af2d6668
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-16 23:39:58 +0530

    refractor whole code

---
+++ express_bookApi/controllers/book.controller.js
@@ -0,0 +1,51 @@
+const { BOOKS } = require('../models/book');
+exports.getAllBooks = function(req,res){
+    res.json(BOOKS)
+}
+
+exports.getAllBooksById = function(req,res){
+  const id = parseInt(req.params.id);
+  const book = BOOKS.find((e) => e.id === id);
+  if (isNaN(id)) {
+    return res.status(400).json({ error: `id must be of type number` });
+  }
+  if (!book) {
+    return res.status(404).json({
+      error: `Book with id ${id} is not found`,
+    });
+  }
+  return res.status(200).json(book);
+};
+
+exports.createBook = function(req, res){
+    const { title, author, price, available } = req.body;
+  if (!title || title === "") {
+    return res.status(400).json({ error: `title is required` });
+  }
+  if (!author || author === "") {
+    return res.status(400).json({ error: `author is required` });
+  }
+  if (!price || price === "") {
+    return res.status(400).json({ error: `price is required` });
+  }
+  if (!available || available === "") {
+    return res.status(400).json({ error: `available is required` });
+  }
+  const id = BOOKS.length + 1;
+  const book = { id, title, author, price, available };
+  BOOKS.push(book);
+  return res.status(201).json({ message: `Book created succesfully` });
+};
+
+exports.deleteBookById = function(req,res){
+    const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).json({ error: `id must be a number` });
+  }
+  const indexToDelete = books.findIndex((e) => e.id === id);
+  if (indexToDelete < 0) {
+    return res.status(404).json({ error: `Book with ${id} is not exist` });
+  }
+  BOOKS.splice(indexToDelete, 1);
+  return res.status(200).json({ message: `Book deleted with the id ${id}` });
+};
--- express_bookApi/routes/route.book.js
+++ express_bookApi/routes/route.book.js
@@ -1,59 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
-const { BOOKS } = require("../db/book");
+const controller = require('../controllers/book.controller')
 
-router.get("/", (req, res) => {
-  res.json(BOOKS);
-});
-
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const book = BOOKS.find((e) => e.id === id);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: `id must be of type number` });
-  }
-  if (!book) {
-    return res.status(404).json({
-      error: `Book with id ${id} is not found`,
-    });
-  }
-  return res.status(200).json(book);
-});
-
-//create book
-router.post("/", (req, res) => {
-  const { title, author, price, available } = req.body;
-  if (!title || title === "") {
-    return res.status(400).json({ error: `title is required` });
-  }
-  if (!author || author === "") {
-    return res.status(400).json({ error: `author is required` });
-  }
-  if (!price || price === "") {
-    return res.status(400).json({ error: `price is required` });
-  }
-  if (!available || available === "") {
-    return res.status(400).json({ error: `available is required` });
-  }
-  const id = BOOKS.length + 1;
-  const book = { id, title, author, price, available };
-  BOOKS.push(book);
-  return res.status(201).json({ message: `Book created succesfully` });
-});
-
-//Delete book by id
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: `id must be a number` });
-  }
-  const indexToDelete = books.findIndex((e) => e.id === id);
-  if (indexToDelete < 0) {
-    return res.status(404).json({ error: `Book with ${id} is not exist` });
-  }
-  BOOKS.splice(indexToDelete, 1);
-  return res.status(200).json({ message: `Book deleted with the id ${id}` });
-});
+router.get(controller.getAllBooks);
+router.get('/:id', controller.getAllBooksById)
+router.post("/", controller.createBook)
+router.delete("/:id", controller.deleteBookById)
+  
 
 module.exports = router;
