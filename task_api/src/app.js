9953291975baef729a3f67fb1bfd7caac97a75e4
bappaditya92 <bappadityasinha.92@gmail.com>
2026-02-26 12:05:27 +0530

    add get api of notes app

--- task_api/src/app.js
+++ task_api/src/app.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const notes = [];
 
-app.post('/notes', (req,res)=>{
-   //res.end('from create notes api');
-   console.log(req.body)
+//create notes api
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(201).json({
+    message: "notes created succesfully...",
+  });
+});
+
+
+//get all notes
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        message:"notes fetched succesfully",
+        notes:notes
+    })
 })
 
 module.exports = app;
